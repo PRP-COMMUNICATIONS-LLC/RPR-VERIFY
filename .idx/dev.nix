7478ce69677cfb6a1bc7d94974fa032a16baf343
butterdime <butterdime@gmail.com>
2025-12-05 14:25:34 +0000

    Fix Node v22.12+ for Angular CLI

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,7 +1,13 @@
 {pkgs}: {
   channel = "stable-24.05";
   packages = [
-    pkgs.nodejs_22
+    (pkgs.nodejs_22.overrideAttrs (old: {
+      version = "22.12.0";
+      src = pkgs.fetchurl {
+        url = "https://nodejs.org/dist/v22.12.0/node-v22.12.0-linux-x64.tar.xz";
+        sha256 = "0351224f605153333469197a3989f66e017a58c634a7051a24367b3f4604a373";
+      };
+    }))
   ];
   idx.extensions = [
     "angular.ng-template"
