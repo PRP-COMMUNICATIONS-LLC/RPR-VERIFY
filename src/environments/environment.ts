3650d4d6ce79203f0131c2568e6afddacf5e9390
puvansivanasan <236526830+Butterdime@users.noreply.github.com>
2025-12-14 22:18:47 +0800

    FIX: Restored new OAuth Client ID following accidental deletion.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
     appId: "1:794095666194:web:53d30da820b709635844cb"
   },
   googleDrive: {
-    clientId: "794095666194-av4h9j5798m3vjlj3amo7csjir2uig7i.apps.googleusercontent.com",
+    clientId: "794095666194-fimj7pc39nmugc4rqt7bd3k490mrsu3q.apps.googleusercontent.com",
     discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
     scopes: [
       'https://www.googleapis.com/auth/drive.file',
