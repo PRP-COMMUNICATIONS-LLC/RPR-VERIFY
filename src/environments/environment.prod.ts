d83d4f79356e9f9d6018a3e8f460695f8ef93a88
puvansivanasan <236526830+Butterdime@users.noreply.github.com>
2025-12-14 22:18:47 +0800

    FIX: Restored new OAuth Client ID following accidental deletion.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,20 @@
 export const environment = {
-        production: true,
-        apiUrl: 'https://rpr-verify-794095666194.asia-southeast1.run.app',
-        firebase: {
-          apiKey: "AIzaSyAcVS96uBW7BW8tw8jeZVV556VqIz1MWKc",
-          authDomain: "rpr-verify-b.firebaseapp.com",
-          projectId: "rpr-verify-b",
-          storageBucket: "rpr-verify-b.firebasestorage.app",
-          messagingSenderId: "794095666194",
-          appId: "1:794095666194:web:53d30da820b709635844cb"
-        },
-        googleDrive: {
-          clientId: "794095666194-av4h9j5798m3vjlj3amo7csjir2uig7i.apps.googleusercontent.com",
-          discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-          scopes: [
-            'https://www.googleapis.com/auth/drive.file',
-            'https://www.googleapis.com/auth/drive.appdata'
-          ]
-        }
-      };
+  production: true,
+  apiUrl: 'https://rpr-verify-794095666194.asia-southeast1.run.app',
+  firebase: {
+    apiKey: "AIzaSyDSv50wy6g0TlhJoEa2gldfDgKNxSDkyEk",
+    authDomain: "rpr-verify-b.firebaseapp.com",
+    projectId: "rpr-verify-b",
+    storageBucket: "rpr-verify-b.firebasestorage.app",
+    messagingSenderId: "794095666194",
+    appId: "1:794095666194:web:53d30da820b709635844cb"
+  },
+  googleDrive: {
+    clientId: "794095666194-fimj7pc39nmugc4rqt7bd3k490mrsu3q.apps.googleusercontent.com",
+    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+    scopes: [
+      'https://www.googleapis.com/auth/drive.file',
+      'https://www.googleapis.com/auth/drive.appdata'
+    ]
+  }
+};
