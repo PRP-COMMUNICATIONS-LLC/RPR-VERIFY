9655f3216e9462df90a79659a35c62f288886d2c
puvansivanasan <236526830+Butterdime@users.noreply.github.com>
2025-12-15 11:51:17 +0800

    chore: migrate to unified Project A (rpr-verify-b)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,15 +2,15 @@ export const environment = {
   production: true,
   apiUrl: 'https://rpr-verify-794095666194.asia-southeast1.run.app',
   firebase: {
-    apiKey: "AIzaSyCsKFdoy-L1jq9dNyqCVIKXiaN-lQt0_7o",
-    authDomain: "rpr-verify-05901876-be7b3.firebaseapp.com",
-    projectId: "rpr-verify-05901876-be7b3",
-    storageBucket: "rpr-verify-05901876-be7b3.firebasestorage.app",
-    messagingSenderId: "255484727596",
-    appId: "1:255484727596:web:f9daf5379818789d375c89"
+    apiKey: "AIzaSyDSv50wy6g0TlhJoEa2gldfDgKNxSDkyEk",
+    authDomain: "rpr-verify-b.firebaseapp.com",
+    projectId: "rpr-verify-b",
+    storageBucket: "rpr-verify-b.firebasestorage.app",
+    messagingSenderId: "794095666194",
+    appId: "1:794095666194:web:53d30da820b709635844cb"
   },
   googleDrive: {
-    clientId: "255484727596-gp0imjqjj1f1k2bnpm79ak2vvrkpqbb4.apps.googleusercontent.com",
+    clientId: "794095666194-fimj7pc39nmugc4rqt7bd3k490mrsu3q.apps.googleusercontent.com",
     discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
     scopes: [
       'https://www.googleapis.com/auth/drive.file',
