bd6b78efe9980fbdebd841be66bc8b8968f22157
Butterdime <236526830+Butterdime@users.noreply.github.com>
2026-01-02 17:03:54 +0800

    fix: Add Angular test configuration for CI/CD - explicit project name, Karma setup, and ChromeHeadless

---
+++ frontend/karma.conf.js
@@ -0,0 +1,36 @@
+// Karma configuration file for Angular testing
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // Jasmine configuration options
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/rpr-verify'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: true,
+    singleRun: true, // Set to true for CI/CD
+    autoWatch: false // Set to false for CI/CD
+  });
+};
